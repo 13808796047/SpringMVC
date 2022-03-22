c3efa67bc3f71f85485531cd6cd109ab819ba993
Summer <511391805@qq.com>
2022-03-22 19:19:53 +0800

    :seedling:SpringMVC项目搭建成功

---
+++ SpringMVC_Test01/src/main/java/com/summer/controller/HomeController.java
@@ -0,0 +1,16 @@
+package com.summer.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+/**
+ * @author Summer
+ * @date 2022/3/22 19:14
+ */
+@Controller
+public class HomeController {
+    @GetMapping("/")
+    public String home() {
+        return "success.jsp";
+    }
+}
